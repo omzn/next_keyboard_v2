726730686be0cead7a510117f79469894ef0a3da
Osamu Mizuno <o-mizuno@kit.ac.jp>
2023-12-12 08:27:00 +0900

    use rgb matrix

--- rules.mk
+++ rules.mk
@@ -6,5 +6,8 @@ OPT_DEFS += -DHAL_USE_I2C=TRUE
 
 WPM_ENABLE = yes  # Enable word per minute counter
 
-RGBLIGHT_ENABLE = yes
+RGBLIGHT_ENABLE = no
+
+RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
 WS2812_DRIVER = vendor
